6af1addb548d25f2a6b9da42108cb1fa5bfe8683
YutaroNagumo <nagubado4649@icloud.com>
2022-11-06 22:18:09 +0900

    1106

--- pages/NftUploader.jsx
+++ pages/NftUploader.jsx
@@ -1,22 +1,19 @@
 // NftUploader.jsx
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link';
 import { Button, TextField } from "@mui/material";
 import React from "react";
-import { useEffect, useState } from 'react'
-// import "./NftUploader.css";
-import { ethers }  from "ethers";
+import { ethers } from "ethers";
 import Web3Mint from "./Web3Mint.json";
 import { Web3Storage } from 'web3.storage';
+import { useEffect, useState } from "react";
 
-const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDdENzUxY0QyNzA0QzQzNTU2ZjE3ZTNkNDZlYTBBNzRjNDkwQjFBRGEiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NjczMzAyMzU0MDQsIm5hbWUiOiJmaXJzdCJ9.zR2IeP-Wy2dm1PR_hdmRUtBrfYptGESX4YeIW612Xek"
-var url = "";
-
-const NftUploader = () => {
-  /*
-   * ユーザーのウォレットアドレスを格納するために使用する状態変数を定義します。
-   */
+export default function Home() {
   const [currentAccount, setCurrentAccount] = useState("");
   /*この段階でcurrentAccountの中身は空*/
-  console.log("currentAccount: ", currentAccount);
+  //console.log("currentAccount: ", currentAccount);
   const checkIfWalletIsConnected = async () => {
     const { ethereum } = window;
     if (!ethereum) {
@@ -67,24 +64,8 @@ const NftUploader = () => {
     }
   };
 
-  const imageToNFT = async (e) => {
-    const client = new Web3Storage({ token: API_KEY })
-    const image = e.target
-    console.log(image)
-
-    const rootCid = await client.put(image.files, {
-        name: 'experiment',
-        maxRetries: 3
-    })
-    const res = await client.get(rootCid) // Web3Response
-    const files = await res.files() // Web3File[]
-    for (const file of files) {
-      console.log("file.cid:",file.cid)
-      askContractToMintNft(file.cid)
-    }
-  };
-
-  const askContractToMintNft = async (ipfs) => {
+  async function askContractToMintNft  (ipfs, variables) {
+    console.log(ipfs, variables);
     const CONTRACT_ADDRESS =
       "0xfF8053A18f44af056DC718e0A5a344A0B8ccC675";
     try {
@@ -98,6 +79,7 @@ const NftUploader = () => {
           signer
         );
         console.log("Going to pop wallet now to pay gas...");
+        console.log("variables : " ,{variables});
         let name = variables[6]
         let prompt = variables[5]
         let seed = variables[0]
@@ -105,6 +87,12 @@ const NftUploader = () => {
         let width = variables[3]
         let guidance_scale = variables[1]
         let steps = variables[4]
+
+        console.log("name : " ,{name});
+        console.log("prompt : " ,{prompt});
+        console.log("seed : " ,{seed});
+        console.log("height : " ,{height});
+
         let nftTxn = await connectedContract.mintIpfsNFT(name,prompt,ipfs,seed,height,width,guidance_scale,steps);
         console.log("Mining...please wait.");
         await nftTxn.wait();
@@ -118,105 +106,169 @@ const NftUploader = () => {
       console.log(error);
     }
   };
-
-  const [variables, setVariables] = useState([0, 0, 0, 0, 0, "", ""]);
-  const updateVariables = (e) => {
-    
-    setVariables((prevState) => prevState.map((value, index) => (index === 5 ?  e.target.value : value)));
-  };
-  const updateSeedVal = (e) => {
-    
-    setVariables((prevState) => prevState.map((value, index) => (index === 0 ?  e.target.value : value)));
-  };
-  const updateGuidanceScale = (e) => {
-    
-    setVariables((prevState) => prevState.map((value, index) => (index === 1 ?  e.target.value : value)));
-  };
-  const updateHeight = (e) => {
-    
-    setVariables((prevState) => prevState.map((value, index) => (index === 2 ?  e.target.value * 512 : value)));
-  };
-  const updateWidth = (e) => {
-    
-    setVariables((prevState) => prevState.map((value, index) => (index === 3 ?  e.target.value * 512: value)));
-  };
-  const updateSteps = (e) => {
-    
-    setVariables((prevState) => prevState.map((value, index) => (index === 4 ?  e.target.value : value)));
-  };
-
-  const updateName = (e) => {
-    
-    setVariables((prevState) => prevState.map((value, index) => (index === 6 ?  e.target.value : value)));
-  };
   
+const [variables, setVariables] = useState(['111111', '2', '512', '512', '5', "テスト", "テスト"]);
+const updatePrompt = (e) => {
+  
+  setVariables((prevState) => prevState.map((value, index) => (index === 5 ?  e.target.value : value)));
+};
+const updateSeedVal = (e) => {
+  
+  setVariables((prevState) => prevState.map((value, index) => (index === 0 ?  e.target.value : value)));
+};
+const updateGuidanceScale = (e) => {
+  
+  setVariables((prevState) => prevState.map((value, index) => (index === 1 ?  e.target.value : value)));
+};
+const updateHeight = (e) => {
+  
+  setVariables((prevState) => prevState.map((value, index) => (index === 2 ?  e.target.value * 64 : value)));
+};
+const updateWidth = (e) => {
+  
+  setVariables((prevState) => prevState.map((value, index) => (index === 3 ?  e.target.value * 64: value)));
+};
+const updateSteps = (e) => {
+  
+  setVariables((prevState) => prevState.map((value, index) => (index === 4 ?  e.target.value : value)));
+};
 
-  let imgsrc = "https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"
+const updateName = (e) => {
+  
+  setVariables((prevState) => prevState.map((value, index) => (index === 6 ?  e.target.value : value)));
+};
+
+  const[data, setData] = useState("https://i.imgur.com/t1fye4S.jpg")
   const generateImage = async () => {
-    url = 'http://localhost:8000/api/member/'+variables[0]+'/'+variables[1]+'/'+variables[2]+'/'+variables[3]+'/'+variables[4]+'/'+variables[5];
-    const response = await fetch(url, {method: 'get'});
-    const json = response.json();
-    console.log(json)
+    //let url = 'http://127.0.0.1:8000/SDAPI/'+variables[0]+'/'+variables[1]+'/'+variables[2]+'/'+variables[3]+'/'+variables[4]+'/'+variables[5];
+    // let url = "http://127.0.0.1:8000/SDAPI/4294967/7/512/512/6/dogs%20and%20Doooog/"
+    let url = "https://i.imgur.com/t1fye4S.jpg"
+    await fetch(url, {method: 'GET'}).then((res) => res.json()).then((data) => setData(data.link)).catch((error) => {
+      console.error('Error:', error);
+    });
+
+    //一時的にdataをurlと置く
+    data = url;
   };
 
-  
-  
+  //IPFSを用いる際は利用する
+//   // NftUploader.jsx
+// const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDVlRjkzOEMxZDgxYjZkODk1YzBBNjlEQjJiYmMzRkFDNTIyMDYxMUMiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NjUzNjcxNDU1MTAsIm5hbWUiOiJVTkNIQUlOIn0.pT0uSDyETOz8xyCpXPnX1T4w6rmkeekwTckJVcJsHsk"
+// const imageToNFT = async (e) => {
+//   const client = new Web3Storage({ token: API_KEY })
+//   const image = data
+
+//   const rootCid = await client.put(image.files, {
+//       name: 'experiment',
+//       maxRetries: 3
+//   })
+//   const res = await client.get(rootCid) // Web3Response
+//   const files = await res.files() // Web3File[]
+//   for (const file of files) {
+//     console.log("file.cid:",file.cid)
+//     askContractToMintNft(file.cid)
+//   }
+// }
+
 
   const renderNotConnectedContainer = () => (
-      <button onClick={connectWallet} className="cta-button connect-wallet-button">
-        Connect to Wallet
-      </button>
-    );
-  /*
-   * ページがロードされたときに useEffect()内の関数が呼び出されます。
-   */
+    <button onClick={connectWallet} className="cta-button connect-wallet-button">
+     Connect to Wallet
+    </button>
+  );
+
+
   useEffect(() => {
     checkIfWalletIsConnected();
   }, []);
 
   return (
-    
-    <div>
-      <div className="outerBox">
-        {currentAccount === "" ? (
-          renderNotConnectedContainer()
-          ) : (
+
+    <div className={styles.container}>
+      {currentAccount === "" ? (
+        renderNotConnectedContainer()
+      ) : (
         <p>If you choose image, you can mint your NFT</p>
-        )}
-      <div className="title">
-      <h2>NFTアップローダー</h2>
-      </div>
-      <div className="nameForm"></div>
-	    
-		    <p>呪文を作成</p>
-		    <input type="text" name="prompt" size="90" onChange={updateVariables}/>
-        <input type="range" name="prompt" size="90" onChange={updateSeedVal} min="0" max="4294967295"/>
-        <input type="range" name="prompt" size="90" onChange={updateGuidanceScale} min="0" max="20"/>
-        <input type="range" name="prompt" size="90" onChange={updateHeight}min="1" max="4"/>
-        <input type="range" name="prompt" size="90" onChange={updateWidth}min="1" max="4"/>
-        <input type="range" name="prompt" size="90" onChange={updateSteps} min="5" max="30"/>
-	    
-      
-      <p>{variables}</p>
-      <Button onClick={generateImage}>
-      画像を生成
-      </Button>
-      {imgsrc === "" ? (
-          renderNotConnectedContainer()
-          ) : (
-        <img 
-        src = {imgsrc}
-        alt="new"
-        />
-        )}
+      )}
 
-      <p><input type="text" size="90" onChange={updateName}/></p>
-      <Button onClick={generateImage}>
-      画像を生成
-      </Button>
-      </div> 
-      </div>
-  );
-};
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="Generated by create next app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-export default NftUploader;
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+        AI絵画を作る
+        </h1>
+        <Button onClick={connectWallet} className="cta-button connect-wallet-button">
+          Walletを接続
+        </Button>
+
+        <p className={styles.description}>
+        Stable Diffusionを用いてAI絵画を実際に作ることができます。作品はブロックチェーン上に保存されます。
+        </p>
+
+        <div className={styles.grid}>
+
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.outerBox}
+          >
+           <img 
+                src = {data}
+                alt="new"
+               />
+          <h2>呪文を入力</h2>
+          <p><input type="text" name="prompt" size="90" onChange={updatePrompt}/></p>
+          <fieldset>
+		        
+            <p>Seed: {variables[0]}</p>
+            <p><input type="range" name="prompt" size="200" onChange={updateSeedVal}min="100" max="4294967292"/></p>
+            <p>Guidance Scale: {variables[1]}</p>
+            <p><input type="range" name="prompt" size="200" onChange={updateGuidanceScale}min="1" max="20"/></p>
+            <p>Height: {variables[2]}</p>
+            <p><input type="range" name="prompt" size="200" onChange={updateHeight} min="8" max="16"/></p>
+            <p>Width: {variables[3]}</p>
+            <p><input type="range" name="prompt" size="200" onChange={updateWidth} min="8" max="16"/></p>
+            <p>Steps: {variables[4]}</p>
+            <p><input type="range" name="prompt" size="200" onChange={updateSteps} min="5" max="10"/></p>
+
+            
+	        </fieldset>
+          <Button onClick={generateImage}>
+              画像を生成
+          </Button>
+
+          <p>
+          名前をつける
+          <p><input type="text" name="prompt" size="90" onChange={updateName}/></p>
+          <Button onClick={askContractToMintNft(data, variables)}>
+          NFTにする
+
+          </Button>
+          </p>
+
+    
+       
+          </a>
+          
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
+    </div>
+  )
+}
